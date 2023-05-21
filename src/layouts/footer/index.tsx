fea0190098a543d7efcffbb17d859d07fa84ae47
vikmay <vikmaykut@gmail.com>
2023-05-21 19:59:15 +0300

    footer nav and logo

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <>
-    
+      <div>
+      <Image
+        src="/../../../public/Logo.png"
+        alt="Logo"
+        width={500}
+        height={300}
+      />
+        <nav>
+          <ul>
+            <li>
+              <a href="location1.html">Location 1</a>
+            </li>
+            <li>
+              <a href="location2.html">Location 2</a>
+            </li>
+            <li>
+              <a href="location3.html">Location 3</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Header from "@/layouts/header";
+import Footer from "@/layouts/footer";
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,6 +8,7 @@ export default function Document() {
       <body>
         <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>
