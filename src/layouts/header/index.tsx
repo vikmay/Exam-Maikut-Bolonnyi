7aa02b8927d2dc4e15fb12f6c8a2771d23006efc
Oleh <120975916+Olehovych@users.noreply.github.com>
2023-05-21 14:22:41 +0300

    header_styles

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-
+import Link from "next/link";
+// Styles //
+import s from "@/layouts/header/index.module.scss";
+// Pages //
+import About from "@/pages/about";
+import Catalog from "@/pages/catalog";
 const Header = () => {
   return (
     <>
-      <header>
-        <nav></nav>
+      <header className={s.header}>
+        <div className={s.logo}>
+          <Link href="/">LOGO</Link>
+        </div>
+        <nav className={s.nav}>
+          <ul>
+            <li>
+              <Link href="catalog">Каталог</Link>
+            </li>
+            <li>
+              <Link href="about">Про нас</Link>
+            </li>
+            <li>
+              <Link href="">FAQ</Link>
+            </li>
+            <li>
+              <Link href="">Контакти</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="action__bar"></div>
       </header>
     </>
   );
