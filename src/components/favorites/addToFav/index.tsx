ba9b8291bdd004724ef18f279e9bd22f0969a7ec
vikmay <vikmaykut@gmail.com>
2023-06-03 01:55:06 +0300

    fav not complete

--- src/components/favorites/addToFav/index.tsx
+++ src/components/favorites/addToFav/index.tsx
@@ -1,5 +1,8 @@
 import { useDispatch } from "react-redux";
-import { addToFavorites } from "@/../../store/features/favoritesSlice";
+import {
+  addToFavorites,
+  removeFromFavorites,
+} from "@/../../store/features/favoritesSlice";
 import Image from "next/image";
 import s from "./index.module.scss";
 import { Product } from "@/../../interfaces";
@@ -7,14 +10,17 @@ import { Product } from "@/../../interfaces";
 interface Props {
   product: Product;
   id: number;
-  
 }
 
 const AddToFavBtn = ({ product, id }: Props) => {
   const dispatch = useDispatch();
 
   const handleFavoriteClick = () => {
-    dispatch(addToFavorites(product));
+    if (product.isFavorite) {
+      dispatch(removeFromFavorites(product));
+    } else {
+      dispatch(addToFavorites(product));
+    }
   };
 
   return (
@@ -23,9 +29,7 @@ const AddToFavBtn = ({ product, id }: Props) => {
         <button className={s.fav_btn} onClick={handleFavoriteClick}>
           <Image
             src={
-              product.isFavorite
-                ? "@/../../public/images/likes.svg"
-                : "@/../../public/images/likes.svg"
+              product.isFavorite ? "/images/fheart.svg" : "/images/heart.svg"
             }
             alt="heart_image"
             width={20}
