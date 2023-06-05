06f7382fbce9d542c19794682bef20efd6df8c93
vikmay <vikmaykut@gmail.com>
2023-06-05 23:52:39 +0300

    favorites are ready

--- src/components/favorites/favItemCard/index.tsx
+++ src/components/favorites/favItemCard/index.tsx
@@ -1,45 +1,54 @@
-import { FavItem } from "@/../../interfaces";
+// import necessary libraries, components and styles
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { removeFromFavorites } from "@/../../store/features/favoritesSlice";
-import { useAppDispatch } from "@/../../store/store";
 import s from "./index.module.scss";
 import Image from "next/image";
 import CrossButton from "@/utils/crossBtn";
-import { Product } from "@/../../interfaces";
 import AddToCartBtn from "@/components/cart/addToCart";
 
+// import required redux action and hook
+import { removeFromFavorites } from "@/../../store/features/favoritesSlice";
+import { useAppDispatch } from "@/../../store/store";
+
+// import necessary types
+import { Product } from "@/../../interfaces";
+
+// Define the type for component props
 interface Props {
   product: Product;
 }
 
+// FavItemCard component
 const FavItemCard = ({ product }: Props) => {
+  // use the redux dispatch hook
   const dispatch = useAppDispatch();
 
+  // return null if there is no product
   if (!product) {
     return null;
   }
 
+  // component render
   return (
     <Container>
-      <Row className={s.favItem_container}>
-        <Col xs={12} sm={12} md={2} className={s.img_name_wrapper}>
+      <Row className={s.favItem__container}>
+        <Col xs={12} sm={12} md={2} className={s.favItem__imgNameWrapper}>
           <Image
             src={product.images[0]}
             width={100}
             height={100}
             alt={product.title}
-            className={s.image}
+            className={s.favItem__image}
           />
         </Col>
 
         <Col xs={12} sm={12} md={4} lg={3}>
-          <div className={s.title}>{product.title.slice(0, 26)}</div>
-          <div className={s.card_model}>{product.title.slice(28, 64)}</div>
+          <div className={s.favItem__title}>{product.title.slice(0, 26)}</div>
+          <div className={s.favItem__cardModel}>{product.title.slice(28, 64)}</div>
         </Col>
 
         <Col xs={12} sm={4} md={2}>
-          <div className={s.item_price}>{product.price} грн</div>
+          <div className={s.favItem__itemPrice}>{product.price} грн</div>
         </Col>
 
         <Col xs={12} sm={1}>
@@ -49,12 +58,12 @@ const FavItemCard = ({ product }: Props) => {
         <Col xs={12} sm={8} md={4}>
           <AddToCartBtn
             id={product.id}
-            className={s.btn_simple}
+            className={s.btn__simple}
             product={product}
             simple={true}
             btnText="Додати до кошика"
-            increaseClassName={s.increaseBtn}
-            decreaseClassName={s.decreaseBtn}
+            increaseClassName={s.btn__increase}
+            decreaseClassName={s.btn__decrease}
           />
         </Col>
       </Row>
@@ -62,4 +71,5 @@ const FavItemCard = ({ product }: Props) => {
   );
 };
 
+// export the component
 export default FavItemCard;
