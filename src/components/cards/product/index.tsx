a5bb30ffb63bb2f49934dd65d34fc21f538df70b
vikmay <vikmaykut@gmail.com>
2023-06-08 09:34:15 +0300

    test point

--- src/components/cards/product/index.tsx
+++ src/components/cards/product/index.tsx
@@ -19,33 +19,36 @@ const ProductCard = (props: any) => {
 
   return (
     <>
-      
-  
-        <div className={s.card_wrapper}>
-          <div className={s.card_container}>
-            <div className={s.new_fav_container}>
-              {NewLabel()}
-              <AddToFavBtn product={product} id={id} />
-            </div>
+      <div className={s.card_wrapper}>
+        <div className={s.card_container}>
+          <div className={s.new_fav_container}>
+            {NewLabel()}
+            <AddToFavBtn product={product} id={id} />
+          </div>
+          
             <Image
+            className={s.card_image}
               src={product?.images[0]}
               alt={product?.title}
               width={200}
               height={200}
             />
-           <Link href={`/catalog/${product.id}`}> <div className={s.card_title}>{product?.title.slice(0, 26)}</div>
-            <div className={s.card_model}>{product?.title.slice(28)}</div>
-            <div className={s.card__producer}>_{product?.producer}</div></Link>
-            <div className={s.price_cart_container}>
-              <div>
-                <div className={s.card__price}>{product?.price + "грн"}</div>
-                <span className={s.on_stock}>В наявності</span>
-              </div>
-              <AddToCartBtn product={product} id={id} />
+          
+          
+            <div className={s.card_title}>{product?.title.slice(0, 26)}</div>
+          
+          <div className={s.card__producer}>_{product?.producer}</div>
+          <div className={s.card_model}>{product?.title.slice(28)}</div>
+
+          <div className={s.price_cart_container}>
+            <div>
+              <div className={s.card__price}>{product?.price + "грн"}</div>
+              <span className={s.on_stock}>В наявності</span>
             </div>
+            <AddToCartBtn product={product} id={id} />
           </div>
         </div>
-      
+      </div>
     </>
   );
 };
--- src/components/sort/index.tsx
+++ src/components/sort/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import s from "./index.module.scss";
+import Accordion from "@/components/accordion2";
 
 interface SortProps {
   onSortOptionChange: (option: string) => void;
 }
 
 const Sort: React.FC<SortProps> = ({ onSortOptionChange }) => {
-  const [selectedOption, setSelectedOption] = React.useState<string>('Сортувати за');
+  const [selectedOption, setSelectedOption] =
+    React.useState<string>("Сортувати за");
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newOption = event.target.value;
@@ -15,13 +17,18 @@ const Sort: React.FC<SortProps> = ({ onSortOptionChange }) => {
   };
 
   return (
-    <select value={selectedOption} onChange={handleChange}>
-      <option value="Сортувати за">Сортувати за</option>
-      <option value="сортувати від А до Я">сортувати від А до Я</option>
-      <option value="від Я до А">від Я до А</option>
-    </select>
+    <>
+      <select
+        className={s.select}
+        value={selectedOption}
+        onChange={handleChange}
+      >
+        <option className={s.option} value="Сортувати за">Сортувати за</option>
+        <option  className={s.option} value="сортувати від А до Я">Алфавітом від А до Я</option>
+        <option  className={s.option} value="від Я до А">Алфавітом від Я до А</option>
+      </select>
+    </>
   );
 };
 
 export default Sort;
-  
